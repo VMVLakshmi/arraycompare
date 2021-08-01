783dc391f48ca179f2a5ee9d8c203ec1c4362643
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-01 19:53:34 +0530

    14th commit
    Uploading all files

---
+++ ARRAY_COMPARE/src/s/TEST7.java
@@ -0,0 +1,11 @@
+package s;
+
+public class TEST7 {
+public static void main(String args[])	{
+	String str="Priya";
+	char[] cArray = str.toCharArray();
+	for(char c : cArray)
+		System.out.println(c);
+}
+
+}
